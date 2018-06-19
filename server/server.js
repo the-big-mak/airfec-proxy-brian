3e72d7c4b6dbc8c3adab6d8c11dd4c787dc7c76a
briansai <brianwsai@gmail.com>
2018-06-19 10:12:18 -0700

    Updated files for proxy server.  Was unableto get it running on proxy.  Have to change src links in index.html

--- server/server.js
+++ server/server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
+const proxy = require('http-proxy-middleware');
 const app = express();
-const PORT = 3000;
+const PORT = 8081;
 
 app.use('/rooms/:id', express.static(path.join(__dirname, '../client')));
 
-
+app.use('/reviews', proxy({target: "http://ec2-54-173-131-192.compute-1.amazonaws.com"}));
+app.use('/description', proxy({target: 'http://http://airfec-env.tdt2hxkqtm.us-east-1.elasticbeanstalk.com'}));
+app.use('/Photos/:id', proxy({target: 'http://localhost:3003'}));
+app.use('/reservations/:id', proxy({target: 'http://ec2-18-208-163-155.compute-1.amazonaws.com'}));
 
 app.listen(PORT, () => console.log('Listening to port: ', PORT));
